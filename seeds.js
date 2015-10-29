835d59565d5ee56c35e542558f2e7758fd18d3fc
Brett Lyons <blyons@students.naropa.edu>
2015-10-29 11:49:56 -0600

    stats seeds broken

--- seeds.js
+++ seeds.js
@@ -7,14 +7,14 @@ var duels = db.get('duels');
 var waresquirrelStats = db.get('waresquirrel-stats');
 var creatureContracts = db.get('creature-contracts');
 
-var carlyBear = unibears.id(),
-    rubyBear = unibears.id(),
+var carlyBear   = unibears.id(),
+    rubyBear    = unibears.id(),
     charlieBear = unibears.id(),
-    gummyBear = unibears.id(),
-    moneyBear = unibears.id(),
-    egoBear = unibears.id(),
-    kanyeBear = unibears.id(),
-    gerbilBear = unibears.id()
+    gummyBear   = unibears.id(),
+    moneyBear   = unibears.id(),
+    egoBear     = unibears.id(),
+    kanyeBear   = unibears.id(),
+    gerbilBear  = unibears.id()
 
 var carlySq   = waresquirrels.id(),
     rubySq    = waresquirrels.id(),
@@ -76,35 +76,48 @@ Promise.all([
       duels.insert({winnerId: rightColony, loserId: lowColony})
     ])
   }),
-]).then(function () {
+
   creatureContracts.remove().then(function () {
-    colonies.find({}).then(function (colonyDocs) {
-      unibears.find({}).then(function (unibearDocs) {
-        var counter = 0;
-        colonyDocs.forEach(function (colonyDoc) {
-          creatureContracts.insert({
-            timeHrs: Math.random() * 12,
-            creature: unibearDoc._id,
-            honey: Math.random() * 3,
-            colony: unibearDocs[counter++]._id
-          })
-        })
-      })
-  }).then(function () {
-    colonies.find({}).then(function (colonyDocs) {
-      waresquirrels.find({}).then(function (waresquirrelDocs) {
-        var counter = 0;
-        colonyDocs.forEach(function (colonyDoc) {
-          creatureContracts.insert({
-            timeHrs: Math.random() * 12,
-            creature: waresquirrelDoc._id,
-            honey: Math.random() * 3,
-            colony: waresquirrelDocs[counter++]._id
-          })
-        })
-      })
-    })
+    return Promise.all([
+      creatureContracts.insert({time: 1, creature: carlyBear, peanuts: 0, honey: 3, colony: rightColony })
+      creatureContracts.insert({time: 2, creature: rubyBear, peanuts: 0, honey: 3, colony: rightColony })
+      creatureContracts.insert({time: 4, creature: charlieBear, peanuts: 0, honey: 3, colony: leftColony })
+      creatureContracts.insert({time: 6, creature: gummyBear, peanuts: 0, honey: 3, colony: leftColony })
+      creatureContracts.insert({time: 4, creature: egoBear, peanuts: 0, honey: 3, colony: lowColony })
+      creatureContracts.insert({time: 3, creature: kanyeBear, peanuts: 0, honey: 3, colony: lowColony })
+      creatureContracts.insert({time: 2, creature: gerbilBear, peanuts: 0, honey: 3, colony: highColony })
+      creatureContracts.insert({time: 8, creature: moneyBear, peanuts: 0, honey: 3, colony: highColony })
+      creatureContracts.insert({time: 5, creature: carlySq, peanuts: 4, honey: 0, colony: rightColony })
+      creatureContracts.insert({time: 3, creature: rubySq, peanuts: 4, honey: 0, colony: rightColony })
+      creatureContracts.insert({time: 7, creature: charlieSq, peanuts: 4, honey: 0, colony: leftColony })
+      creatureContracts.insert({time: 9, creature: gummySq, peanuts: 4, honey: 0, colony: leftColony })
+      creatureContracts.insert({time: 6, creature: egoSq, peanuts: 4, honey: 0, colony: lowColony })
+      creatureContracts.insert({time: 5, creature: kanyeSq, peanuts: 4, honey: 0, colony: lowColony })
+      creatureContracts.insert({time: 1, creature: gerbilSq, peanuts: 4, honey: 0, colony: highColony })
+      creatureContracts.insert({time: 5, creature: moneySq, peanuts: 4, honey: 0, colony: highColony })
+    ])
+  }),
+// var waresquirrelStats = db.get('waresquirrel-stats');
+  waresquirrelStats.remove({}).then(function () {
+    return Promise.all([
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: carlySq,   duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: rubySq,    duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: charlieSq, duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: gummySq,   duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: moneySq,   duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: egoSq,     duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: kanyeSq,   duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: gerbilSq,  duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: carlySq,   duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: rubySq,    duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: charlieSq, duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: gummySq,   duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: moneySq,   duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: egoSq,     duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: kanyeSq,   duelId: }),
+      waresquirrelStats.insert({meat: 1, garlic: 1, squirrelId: gerbilSq,  duelId: })
+    ])
   })
-}).then(function () {
+]).then(function () {
   db.close();
-})
+});
